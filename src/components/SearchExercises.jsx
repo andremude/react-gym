9cd602b6062055356a2c042784a559d36c545192
Andres Munoz <andremude@hotmail.com>
2023-05-08 21:48:40 -0500

    add fetchData

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 import '../styles/SearchExercises.css'
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const SearchExercises = () => {
 
   const [search, setSearch] = useState('')
 
-  // const handleSearch = async () => {
-  //   if(search) {
-  //     const exercisesData = await fetchData();
-  //   }
-  // }
+  const handleSearch = async () => {
+    if(search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+    }
+  }
 
   return (
     <div className='search-exercises-container'>
@@ -17,7 +19,7 @@ const SearchExercises = () => {
         Should Know</p>
       <div className='search-container'>
         <input type="text" value={search} onChange={(e) => {setSearch(e.target.value.toLowerCase())}} placeholder="Search Exercises" />
-        { <button className='search-btn' /*onClick={handleSearch}*/>Search</button> }
+        { <button className='search-btn' onClick={handleSearch}>Search</button> }
       </div>
     </div>
   )
